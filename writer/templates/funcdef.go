64997c2d9fe0126eae7b51c9f7fba23c2dceea48
Gabriele <gabriele.vaccari@lacuna-space.com>
2026-04-16 12:34:11 +0200

    Add FuncDef documentation

--- writer/templates/funcdef.go
+++ writer/templates/funcdef.go
@@ -2,13 +2,14 @@ package templates
 
 import "strings"
 
+// FuncDef represents the information needed to output mocks based on the interface's methods
 type FuncDef struct {
-	ServiceName  string
-	Name         string
-	Signature    string
-	Return       string
-	Args         string
-	ReturnValues string
+	ServiceName  string // Name of the interface that is being mocked (can be ovverridden by some options)
+	Name         string // Identifier of this function
+	Signature    string // Full parameter list of this function excluding brackets
+	Return       string // Full return parameter list of this function including brackets
+	Args         string // List of function arguments
+	ReturnValues string // List of values that can appear in this function's return statement
 }
 
 func (fd FuncDef) String() string {
